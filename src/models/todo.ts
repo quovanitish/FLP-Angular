9ceae555328fc16ddcba2cc6c386bab4eec9e2ef
quovanitish <nitish.sharma1@quovantis.com>
2021-10-18 18:33:55 +0530

    Refactored todo interface as class

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,7 +1,21 @@
-export interface Todo {
-  title: string,
-  body: string,
-  status: string,
-  createdOn: string,
-  uid: string,
+export class Todo {
+  title: string;
+  body: string;
+  status: string;
+  createdOn: string;
+  uid: string;
+
+  constructor(
+    title: string,
+    body: string,
+    status: string,
+    createdOn: string,
+    uid: string
+  ) {
+    this.title = title;
+    this.body = body;
+    this.status = status;
+    this.createdOn = createdOn;
+    this.uid = uid;
+  }
 }
