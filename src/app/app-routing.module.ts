c2bc8e5bb25bac799217ac82fc3284fe0f19637f
quovanitish <nitish.sharma1@quovantis.com>
2021-10-20 15:54:19 +0530

    Authentication for todo implemented using local storage and router guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { AuthGuard } from "./components/todos/guard/auth.guard";
 import { TodosComponent } from "./components/todos/todos.component";
 
 const routes: Routes = [
   { path: "login", component: LoginFormComponent },
-  { path: "todos", component: TodosComponent },
+  { path: "todos", canActivate: [AuthGuard], component: TodosComponent },
+  { path: "", redirectTo: "LoginFormComponent", pathMatch: "full" },
 ];
 
 @NgModule({
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Login } from "../../../models/userLogin";
 import { Router } from "@angular/router";
+import { TodoService } from "src/app/todo.service";
 
 @Component({
   selector: "app-login-form",
@@ -8,15 +9,15 @@ import { Router } from "@angular/router";
   styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private todoService: TodoService) {}
 
   ngOnInit(): void {}
-
+  
   user = new Login("", "");
-
+  
   // event handler for form submit
   onSubmit(userLoginObj: object) {
-    console.warn(userLoginObj);
+    localStorage.setItem("isLoggedIn", JSON.stringify({loggedIn: true}))
     this.router.navigate(["/todos"]);
   }
 }
