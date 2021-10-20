aca5f87e39c075fcd1f0b01a50874b0153a73091
quovanitish <nitish.sharma1@quovantis.com>
2021-10-20 14:40:45 +0530

    Added login form validation and improved UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodosComponent } from './components/todos/todos.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { TodoService } from './todo.service';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { TodosComponent } from "./components/todos/todos.component";
+import { TodoComponent } from "./components/todo/todo.component";
+import { TodoService } from "./todo.service";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoComponent,
-    LoginFormComponent
+    LoginFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [TodoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Login } from "../../../models/userLogin";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  user = new Login("", "");
+
+  // event handler for form submit
+  onSubmit(userLoginObj: object) {
+    console.warn(userLoginObj);
+    this.router.navigate(["/todos"]);
   }
-
 }
