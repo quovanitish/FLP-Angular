6c0fd006cb03897f769330a9f285bb29104905c7
quovanitish <nitish.sharma1@quovantis.com>
2021-11-16 13:49:40 +0530

    Hash passwords before saving to db

--- src/mongoModel/user.js
+++ src/mongoModel/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,12 +25,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    minlength: 7,
     validate(value) {
       if (value.toLowerCase().includes("password")) {
         throw new Error('Password cannot contain "password"');
       }
     },
   },
+});
+
+// hash plain text passwords before save
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
