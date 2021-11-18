63cf6eec35633d5b37be452078912308442108a8
quovanitish <nitish.sharma1@quovantis.com>
2021-11-18 15:10:20 +0530

    Added owner field for the todo

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -4,18 +4,21 @@ export class Todo {
   status: string;
   createdOn: string;
   uid: string;
+  owner: string;
 
   constructor(
     title: string,
     body: string,
     status: string,
     createdOn: string,
-    uid: string
+    uid: string,
+    owner: string
   ) {
     this.title = title;
     this.body = body;
     this.status = status;
     this.createdOn = createdOn;
     this.uid = uid;
+    this.owner = owner;
   }
 }
