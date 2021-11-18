eca8375e567f87018b8ee00362eef9c9b8c76830
quovanitish <nitish.sharma1@quovantis.com>
2021-11-18 12:15:53 +0530

    Created JWT token interceptor service for requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { TodosComponent } from "./components/todos/todos.component";
 import { TodoComponent } from "./components/todo/todo.component";
-import { TodoService } from "./todo.service";
+import { TodoService } from "./services/todo/todo.service";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
 import { SearchComponent } from "./components/search/search.component";
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TokenInterceptorService } from "./services/token/token-interceptor.service";
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,8 +19,21 @@ import { SearchComponent } from "./components/search/search.component";
     LoginFormComponent,
     SearchComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
-  providers: [TodoService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [
+    TodoService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
